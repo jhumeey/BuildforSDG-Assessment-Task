9edabd3603c06fb9e1240f99f6192462722da297
giselle2323 <yakubuaminat94@gmail.com>
2020-04-11 16:27:57 +0100

    Worked on the first challenge, added the updated changes to the gradr file from the SGD template.

--- src/functions/infections.js
+++ src/functions/infections.js
@@ -1,19 +1,65 @@
-class InfectionsByRequestedTime {
-  static infectionsForImpact(data) {
-    try {
-      return data;
-    } catch (error) {
-      return error.message;
-    }
+// Challenge 1
+// import userData from './data';
+const userData = {
+  data: {
+    region: {
+      name: 'Africa',
+      avgAge: 19.7,
+      avgDailyIncomeInUSD: 4,
+      avgDailyIncomePopulation: 0.73
+    },
+    periodType: 'Months',
+    timeToElapse: 88,
+    reportedCases: 2747,
+    population: 92931687,
+    totalHospitalBeds: 678874
   }
+};
 
-  static infectionsForSevereImpact(data) {
-    try {
-      return data;
-    } catch (error) {
-      return error.message;
-    }
+
+const currentlyInfected = (param) => {
+  try {
+    const { data: { reportedCases } } = param;
+    const currentlyInfectedForImpact = reportedCases * 10;
+    const currentlyInfectedForSevereImpact = reportedCases * 50;
+    return [currentlyInfectedForImpact, currentlyInfectedForSevereImpact];
+  } catch (error) {
+    return error.message;
   }
-}
+};
 
-export default InfectionsByRequestedTime;
+console.log(currentlyInfected(userData));
+
+const convertToDays = (param) => {
+  let days;
+  const { data: { timeToElapse }, data: { periodType } } = param;
+  const setPeriodTypeToLowerCase = periodType.toLowerCase();
+  console.log(setPeriodTypeToLowerCase);
+  switch (setPeriodTypeToLowerCase) {
+    case 'days':
+      days = timeToElapse;
+      break;
+    case 'weeks':
+      days = timeToElapse * 7;
+      break;
+    case 'months':
+      days = timeToElapse * 30;
+      break;
+    default:
+      days = timeToElapse;
+  }
+  return days;
+};
+
+console.log(convertToDays(userData));
+
+const infectionsByTime = () => {
+  try {
+    // const {  } = param;
+    const factor = Math.floor(convertToDays(userData) / 3);
+    return factor;
+  } catch (error) {
+    return error.message;
+  }
+};
+console.log(infectionsByTime());
--- src/functions/severeCases.js
+++ src/functions/severeCases.js
@@ -1,37 +1,15 @@
-class SevereCasesByRequestedTime {
-  static casesForImpact(data) {
-    try {
-      return data;
-    } catch (error) {
-      return error.message;
-    }
+export const infectionsForImpact = (data) => {
+  try {
+    return data;
+  } catch (error) {
+    return error.message;
   }
+};
 
-  static casesForSevereImpact(data) {
-    try {
-      return data;
-    } catch (error) {
-      return error.message;
-    }
+export const infectionsForSevereImpact = (data) => {
+  try {
+    return data;
+  } catch (error) {
+    return error.message;
   }
-
-  static hospitalBedsForImpact(data) {
-    try {
-      // this.casesForSevereImpact(data);
-      return data;
-    } catch (error) {
-      return error.message;
-    }
-  }
-
-  static hospitalBedsForSevereImpact(data) {
-    try {
-      // this.casesForSevereImpact(data);
-      return data;
-    } catch (error) {
-      return error.message;
-    }
-  }
-}
-
-export default SevereCasesByRequestedTime;
+};
